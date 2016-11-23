b1fd223a082a6963c608aa221cf859478e358e5f
tk <masafumi.cascata@gmail.com>
2016-11-24 03:14:26 +0900

    add configfile

--- jobs/favotweet.rb
+++ jobs/favotweet.rb
@@ -1,14 +1,16 @@
 require 'twitter'
 require 'rubygems'
-
+require 'yaml'
 #### Get your twitter keys & secrets:
 #### https://dev.twitter.com/docs/auth/tokens-devtwittercom
 
+twitter_config = YAML.load_file('appconfig.yml')
+
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key = ''
-  config.consumer_secret = ''
-  config.access_token = ''
-  config.access_token_secret = ''
+  config.consumer_key = twitter_config["twitterconfig"]["consumer_key"]
+  config.consumer_secret = twitter_config["twitterconfig"]["consumer_secret"]
+  config.access_token = twitter_config["twitterconfig"]["access_token"]
+  config.access_token_secret = twitter_config["twitterconfig"]["access_token_secret"]
 end
 
 #twitter.update("test")
